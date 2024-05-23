fee6b514cc320e455a9cb546c5d00bf688ff469f
Agata Kamińska <a.fluffypie@gmail.com>
2024-05-23 11:56:39 +0200

    .

---
+++ script.js
@@ -0,0 +1,19 @@
+function createList(array) {
+  if (!Array.isArray(array)) {
+    console.error("Wprowadź dane w formie tablicy.");
+    return;
+  }
+  const ul = document.createElement("ul");
+  ul.id = "list";
+  const body = document.querySelector("body");
+  body.appendChild(ul);
+  const liToAdd = array.map((item) => {
+    const li = document.createElement("li");
+    li.textContent = item;
+    return li;
+  });
+  liToAdd.forEach((li) => {
+    ul.appendChild(li);
+  });
+  return ul;
+}
